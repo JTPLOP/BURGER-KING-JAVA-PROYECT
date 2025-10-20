33787a8e551a3c067a02f11d131b8a2efc31f338
AULA08-XX\mejias.doger25_trian <mejias.doger25@triana.salesianos.edu>
2025-10-20 09:02:47 +0200

    declaración int prueba

--- BurgerKing_ScrumProyect/src/burgerKingV1/Principal.java
+++ BurgerKing_ScrumProyect/src/burgerKingV1/Principal.java
@@ -5,8 +5,11 @@ public class Principal {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		int a=0;
+		
 		System.out.println("Esto es una prueba");
 		System.out.println("Esto es una segunda prueba");
+		
 	}
 
 }
